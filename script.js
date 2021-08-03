80ad6379bd074d8efe3fe824aaa008dbf45921da
Ivan MPR <ivsanmas@gmail.com>
2021-08-03 11:53:29 +0200

    let to const, added focus to loc field

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-let loc = document.getElementById("location");
-let date = document.getElementById("date");
-let amount = document.getElementById("amount");
+const loc = document.getElementById("location");
+const date = document.getElementById("date");
+const amount = document.getElementById("amount");
 const button = document.getElementById("btn");
 const table = document.getElementById("table");
-
+window.addEventListener("load", () => {
+  loc.focus();
+});
 function addNewItem() {
   if (loc.value === "" && date.value === "" && amount.value === "") {
     alert("You must type in something");
   } else if (loc.value === "" || date.value === "" || amount.value === "") {
     alert("Please fill in all fields");
   } else {
-    let newRow = table.insertRow();
-    let firstC = newRow.insertCell(0);
-    let secondC = newRow.insertCell(1);
-    let thirdC = newRow.insertCell(2);
+    const newRow = table.insertRow();
+    const firstC = newRow.insertCell(0);
+    const secondC = newRow.insertCell(1);
+    const thirdC = newRow.insertCell(2);
     firstC.innerText = loc.value;
     secondC.innerText = date.value;
     thirdC.innerText = amount.value;
@@ -21,15 +23,15 @@ function addNewItem() {
     date.value = "";
     amount.value = "";
 
-    let x = document.createElement("span");
+    const x = document.createElement("span");
     x.setAttribute("title", "Delete note");
-    let xInput = document.createTextNode("x");
+    const xInput = document.createTextNode("x");
     x.classList.add("close");
     x.appendChild(xInput);
     thirdC.appendChild(x);
 
     x.onclick = function () {
-      let grandpa = this.parentElement.parentElement;
+      const grandpa = this.parentElement.parentElement;
       grandpa.remove();
     };
   }
