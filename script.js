e1e466844ca6aa1ebd138319f39ce501e716f800
Ivan MPR <ivsanmas@gmail.com>
2021-08-03 12:23:18 +0200

    removed 'onclick' event listener

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const date = document.getElementById("date");
 const amount = document.getElementById("amount");
 const button = document.getElementById("btn");
 const table = document.getElementById("table");
+// ////////////////////////////////////////////////////
 window.addEventListener("load", () => {
   loc.focus();
 });
+// ////////////////////////////////////////////////////
+window.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("close")) return;
+  e.target.parentElement.parentElement.remove();
+});
+// ///////////////////////////////////////////////////
 function addNewItem() {
   if (loc.value === "" && date.value === "" && amount.value === "") {
     alert("You must type in something");
@@ -16,32 +23,26 @@ function addNewItem() {
     const firstC = newRow.insertCell(0);
     const secondC = newRow.insertCell(1);
     const thirdC = newRow.insertCell(2);
+    // //////////////////////////////////////////////
     firstC.innerText = loc.value;
     secondC.innerText = date.value;
     thirdC.innerText = amount.value;
+    // ///////////////////////////////////////////////
     loc.value = "";
     date.value = "";
     amount.value = "";
-
+    // ////////////////////////////////////////////////
     const x = document.createElement("span");
     x.setAttribute("title", "Delete note");
     const xInput = document.createTextNode("x");
     x.classList.add("close");
     x.appendChild(xInput);
     thirdC.appendChild(x);
-
-    x.onclick = function () {
-      const grandpa = this.parentElement.parentElement;
-      grandpa.remove();
-    };
   }
 }
 
 button.addEventListener("click", addNewItem);
-// let deleteItem = document.querySelector('.close');
-// deleteItem.addEventListener('click', function() {
 
-// })
 // let myStorage = window.localStorage;
 // myStorage.setItem('first', 'value')
 // console.log(myStorage);
